19eafc4f286141e27814acd3963f5bce84530c4d
MasterKuei <hank5678tw@gmail.com>
2017-09-13 21:23:32 +0800

    預先登錄 進度更新

--- src/assets/scripts/register.js
+++ src/assets/scripts/register.js
@@ -1,2 +1,18 @@
 var a = 10;
 
+var form = new Vue({
+    el: '#form',
+    data: {
+        show: true
+    },
+    methods: {
+        closeForm: function() {
+            this.show = false;
+        }
+    }
+})
+
+
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x000000, view: document.getElementById('pixiCanvas')});
+var map = PIXI.Sprite.fromImage('assets/images/test.png');
+app.stage.addChild(map);
