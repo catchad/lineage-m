2b3a286bf1702ef2a853ef9234551c8cae3acd51
mosquito chang <bbbb00y@gmail.com>
2017-09-16 21:59:28 +0800

    analysis update to page5

--- src/assets/scripts/analysis.js
+++ src/assets/scripts/analysis.js
@@ -1,1 +1,21 @@
-var a = 10;
+$(".debug button").on("click", function(){
+	$(".show").removeClass("show");
+	$("#"+$(this).data("to")).addClass("show");
+})
+
+
+$(".job-clicker").on("click", function() {
+	$(".job.selected").removeClass("selected");
+	$("."+$(this).data("job")).addClass("selected");
+})
+
+$(".choose-frame").on("click", function() {
+	var stat = $(this).data("stat");
+	if(stat == "old") {
+		$(".new").removeClass("selected").addClass("not-selected");
+		$(".old").removeClass("not-selected").addClass("selected");
+	} else {
+		$(".old").removeClass("selected").addClass("not-selected");
+		$(".new").removeClass("not-selected").addClass("selected");
+	}
+})
