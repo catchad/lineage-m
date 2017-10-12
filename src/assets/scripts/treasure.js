53f106c8d5fab747382cba443a5d56f53cf7a458
mosquito chang <bbbb00y@gmail.com>
2017-10-12 17:51:18 +0800

    update treasure layout

--- src/assets/scripts/treasure.js
+++ src/assets/scripts/treasure.js
@@ -1,3 +1,5 @@
+var isAjaxing = false;
+
 var clipboard = new Clipboard('.btn-copy');
 clipboard.on('success', function(e) {
     e.clearSelection();
@@ -11,20 +13,70 @@ clipboard.on('success', function(e) {
     },800);
 });
 
-// debug用
-$(".debug button").on("click", function(){
-	toPage($(this).data("to"));
-})
-
 $(".btn-login").on("click", function() {
-	if($(".tel-input").val().length && /^[09]{2}[0-9]{8}$/.test($(".tel-input").val())) {
-		toPage(2);
+	var phone = $(".tel-input").val();
+	if ($(".tel-header-selecter").val() == "TW") {
+	    if (!/((?=(09))[0-9]{10})$/g.test(String(phone))) {
+	        if (!/((?=(9))[0-9]{9})$/g.test(String(phone))) {
+	            alertText("手機號碼格式不正確");
+	            return;
+	        }
+	    }
+	} else if ($(".tel-header-selecter").val() == "HK") {
+	    if (!/([0-9]{8})$/g.test(String(phone))) {
+	        alertText("手機號碼格式不正確");
+	        return;
+	    }
 	} else {
-		alertText("手機號碼格式不正確");
+	    if (!/([0-9]{8})$/g.test(String(phone))) {
+	        alertText("手機號碼格式不正確");
+	        return;
+	    }
 	}
+
+	var APIurl = "";
+	var data = "";
+
+
+	var isRegistered;
+	//call api
+	isAjaxing = true;
+	$.ajax({
+	    url: APIurl,
+	    type: 'POST',
+	    headers: {
+	        apikey: $("#apikey").val()
+	    },
+	    contentType: 'application/json; charset=UTF-8',
+	    dataType: 'json',
+	    data: JSON.stringify(data)
+	})
+	.always(function (response) {
+		isAjaxing = false;
+
+		if (response.data == undefined) {
+			//假資料
+			isRegistered = true;
+		} else {
+		    if (response.code == "0000") {
+		    	//do something...
+		    } else {
+		        alertText(response.message);
+		        return;
+		    }
+		}
+
+		if(isRegistered) {
+			//已參加事前登錄
+			toPage(2);
+		} else {
+			//未參加事前登錄
+			alertTextClose("<p>尚未參與「事前登錄」</p><p>請先參與「事前登錄」後，</p><p>重新點擊好友分享的連結進入。</p>");
+		}
+	});
 })
 
-$(".btn-info").on("click", function() {
+$(".info").on("click", function() {
 	$(".info-box").addClass('active');
 	$("html, body").animate({ scrollTop: 0 }, "fast");
 })
@@ -34,6 +86,11 @@ $(".info-box .close").on("click", function() {
 	$("html, body").animate({ scrollTop: 0 }, "fast");
 })
 
+
+// debug用
+$(".debug button").on("click", function(){
+	toPage($(this).data("to"));
+})
 // change page
 function toPage(pageIndex) {
 	$(".show").removeClass("show");
